5f13b94e7d534e7f1dddf24f3b7483999e4b2f75
KatsuyaKawabe <pokopenkatuya@gmail.com>
2017-07-31 14:07:04 +0200

    fix

---
+++ configure.sh
@@ -0,0 +1,6 @@
+export PATH=/usr/local/opt/openssl/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/opt/openssl/lib:$LD_LIBRARY_PATH
+export CPATH=/usr/local/opt/openssl/include:$CPATH
+export LDFLAGS=-L/usr/local/opt/openssl/lib
+export CPPFLAGS=-I/usr/local/opt/openssl/include
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig:$PKG_CONFIG_PATH
